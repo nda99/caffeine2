1f5b36d72b46dd3bd731445a2f01926d850569d4
Nathan <nd99@hw.ac.uk>
2019-02-03 18:31:43 +0000

    Added variables, setters and getters

--- src/system/Order.java
+++ src/system/Order.java
@@ -1,10 +1,51 @@
 package system;
+import java.sql.Timestamp;
+import java.util.HashMap;
 
 public class Order {
+	
+	private int orderID;
+	public int getOrderID() {
+		return orderID;
+	}
+
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+
+	public Timestamp getTime() {
+		return time;
+	}
+
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public HashMap<String, Integer> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(HashMap<String, Integer> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	private Timestamp time; 
+	//private Customer customer;
+	private double total;
+	private HashMap<String,Integer> orderItems;
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		// This line is written by Alaa to test 
 	}
 
+	
 }
