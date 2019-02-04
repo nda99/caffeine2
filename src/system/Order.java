8d32640b3ad666a5f5ad74d9cf969a9d99c353ec
Nathan <nd99@hw.ac.uk>
2019-02-04 15:54:41 +0000

    Improvements on Order Class

--- src/system/Order.java
+++ src/system/Order.java
@@ -46,6 +46,54 @@ public class Order {
 		System.out.println("Hello World");
 		// This line is written by Alaa to test 
 	}
-
+	
+	/**
+	 * Add items order hashmap memory
+	 * @param item containing item name
+	 * @param quantity containing how many of this item have been ordered 
+	 */
+	public void addItem(String item, int quantity ) {
+		orderItems.put(item, quantity);
+	}
+	
+	/**
+	 * Removes an item from the order (just subtract one if multiple)
+	 * @param item Name of item
+	 */
+	public void deleteItem(String item) {
+		int quantity = orderItems.get(item);
+		if (quantity>0) {
+			orderItems.put(item, quantity - 1) ;
+		}
+	}
+	
+	/**
+	 * Calculate order total price with discount voucher
+	 * @param voucher
+	 * @return order total price
+	 */
+	public double calculateTotal(String voucher) {
+		double discount = validateDiscount(voucher);
+		return discount;
+	}
+	
+	/**
+	 * Calculate order total price when no discount voucher is provided
+	 * @return order total price
+	 */
+	public double calculateTotal() {
+		double price = 0;
+		return price;
+	}
+	
+	/**
+	 * Checks if discount voucher is valid
+	 * @param voucher
+	 * @return discount percentage
+	 */
+	private double validateDiscount(String voucher){
+		double discount = 0;
+		return discount;
+	}
 	
 }
