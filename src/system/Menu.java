96f0a3beaf680e3aeaa78dedf9ae89ce5518e212
Emma Wood <elw1@hw.ac.uk>
2019-02-07 11:19:03 +0000

    add menu

--- src/system/Menu.java
+++ src/system/Menu.java
@@ -1,5 +1,39 @@
 package system;
 
-public class Menu {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-}
+public class Menu {
+	public static void main(String[] args){
+// add all items from list 
+	MenuItem allmenuItemList;
+	allmenuItemList = new MenuItem();
+	allmenuItemList.readFile("/Users/emmawood/Documents/git_caffine/caffeine/menuItems.csv");
+	
+	
+
+
+	class MeniItemList {
+		private ArrayList<MenuItem> menuItemList;
+	public  MeniItemList() {
+		this.menuItemList = new ArrayList<MenuItem>();	
+	}
+
+	
+		
+		//print table of competitors 
+		System.out.println(allmenuItemList.getTableOfItemsInMenu());
+	
+		
+		
+	
+	}
+	}}
+
+
