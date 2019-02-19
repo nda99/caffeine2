0a8f1116228c5eabf006cd89f978668a59b65a82
aat4 <44607697+aat4@users.noreply.github.com>
2019-02-19 09:38:08 +0000

    didn't change it

--- src/system/Order.java
+++ src/system/Order.java
@@ -185,10 +185,11 @@ public class Order {
 		//MEAL DEAL: COLD DRINK + SANDWICH + PASTRY = £5.99
 		boolean cold=false, sand=false, pastry=false;
 		double cPrice=0.0, sPrice=0.0, pPrice=0.0;
+		System.out.println(orderItems);
 		for (Map.Entry<MenuItem,Integer> m: orderItems.entrySet()) {
 
 
-			if(Menu.getItem(m.getKey().toString()).getCategory()==Category.COLDDRINK) {
+			if(Menu.getItem(m.getKey().toString()).getCategory().equals(Category.COLDDRINK)){
 				System.out.println(m.getKey().toString());
 				cold=true;
 				cPrice = Menu.getItem(m.getKey().toString()).getPrice();
