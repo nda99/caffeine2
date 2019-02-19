48f8b9e4b994c1193e757cdafe2005a8217e02bc
Emma Wood <elw1@hw.ac.uk>
2019-02-19 11:26:32 +0000

    eddit to classes and new methods added

--- src/system/Menu.java
+++ src/system/Menu.java
@@ -1,11 +1,8 @@
 package system;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.math.BigDecimal;
+import java.nio.channels.FileChannel;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -14,197 +11,207 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Menu {
-	
-	
-	/* Set<Category> categorySet = new HashSet<Category>();
+
+    static Map<String, MenuItem>  menuItems = new HashMap<String, MenuItem>();
+    private String menuFile;
 
 
-	
-		
-		//add item to hashset
-		public void addCategory(Category category) {
-			categorySet.add(category);
-	}
-		// delete item from hashset
-		public void deleteCategory(Category category) {
-			categorySet.remove(category);
-		}
-*/
 
-		//create the hashset
+    //add item to hashset
+    public static void addItem(String name, MenuItem menuItem) {
+        menuItems.put(name,menuItem);
+    }
+    // delete item from hashset
+    public static void deleteItem(String name) {
+        menuItems.remove(name);
+    }
 
 
-	static Map<String, MenuItem>  menuItems = new HashMap<String, MenuItem>();
 
 
-			
-			//add item to hashset
-			public static void addItem(String name, MenuItem menuItem) {
-				menuItems.put(name,menuItem);
-		}
-			// delete item from hashset
-			public static void deleteItem(String name) {
-				menuItems.remove(name);
-			}
+    public static MenuItem getItem(String name) {
+        return menuItems.get(name);
+    }
 
-			
 
-	
-public static MenuItem getItem(String name) {
-	return menuItems.get(name);
+
+// read from csv file
+    public void readFile(String filename) {
+        this.menuFile = filename;
+        try {
+            Scanner inputFromFile = new Scanner(new File(filename));
+
+            //checks if there is a line in the scanner
+            while (inputFromFile.hasNextLine()) {
+
+                // reads the line and assign the data read from the line to a local variable
+                String datainLine = inputFromFile.nextLine();
+
+                // Checks if the line has any data, if no data exists in the line then stop the reading and close the file
+                if (datainLine.length() != 0) {
+
+                    //Send the read data from a line for further processing to a function called processDataInLine
+                    processLine(datainLine);
+                }
+            }
+            inputFromFile.close(); //close the file
+
+        }
+        catch (FileNotFoundException fnf) {
+            System.out.println("File not found ");
+        }
+    }
+
+
+    private Category translateCategory(String test) {
+        if(test == "hot drink") {
+            return Category.HOTDRINK;
+        }
+
+        else if (test == "cold drink") {
+            return Category.COLDDRINK;
+        }
+
+        else if (test == "Pastries") {
+            return Category.PASTRIES;
+        }
+
+        else {
+            return Category.SANDWICH;
+        }
+
+
+    }
+
+    public void processLine(String line) {
+        String itemNumber = "";
+        String itemPrice = "";
+        try {
+            String parts [] = line.split(",");
+            String itemName = parts [1];
+            itemNumber = parts [0];
+            System.out.println(parts[0]);
+            System.out.println(parts[1]);
+            System.out.println(parts[2]);
+            System.out.println(parts[3]);
+            System.out.println(parts[4]);
+
+
+
+
+            //Integer itemNo = Integer.parseInt(itemNumber);
+
+            Category category = translateCategory(parts[2]);
+
+
+            itemPrice = parts [3];
+            double Price = Double.parseDouble(parts[3]);
+
+
+            
+            int No = Integer.parseInt(itemNumber);
+            int quantity = Integer.parseInt(parts[4]);
+            //	public MenuItem(int itemNumber, String itemName, Category itemCategory, double price ) {
+
+            MenuItem item = new MenuItem(No,itemName,category,Price, quantity);
+            menuItems.put(itemName, item);
+        }
+        finally{}
+
+    }
+
+
+
+    // write text to a file
+    public void writeToFile(String filename, String report) {
+        FileWriter fw;
+        try {
+            fw = new FileWriter(filename);
+            fw.write("The report\n");
+            fw.write(report);
+            fw.close();
+        }
+        catch(FileNotFoundException fnf) {
+            System.out.println(filename + "Not found");
+            System.exit(0);
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.exit(1);
+        }
+    }
+    //print to a file
+    public void printToFile(String filename) throws IOException {
+        PrintWriter pw;
+        try {
+            pw = new PrintWriter(filename);
+            pw.print("The report\n" );
+            pw.close();
+        }
+        catch(FileNotFoundException pfn){
+            System.out.print(filename + "Not found");
+            System.exit(2);
+
+        }
+    }
+
+
+    /**
+     * Update the file to record the number of points, lazy version, erases the previous file and write it again.
+     * This method should always be used before finishing the program to save the changes.
+     * @return true if it succeeded, false else
+     */
+    public boolean updateFile(){
+        String backUpFile = "MenuTempBackup.csv";
+        // Copying previous customer file before erasing it
+        copyFiles(this.menuFile, backUpFile);
+        //deleting old customer file
+        new File(this.menuFile).delete();
+
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(this.menuFile), "utf-8"))) {
+            String newline = System.getProperty("line.separator");
+
+            //fill new file with current data in the Hashmap
+            writer.write("item no,item name,category,price,quantity" + newline);
+            for (HashMap.Entry<String, MenuItem> custTemp : menuItems.entrySet()) {
+                MenuItem miTemp = custTemp.getValue();
+                writer.write( miTemp.getNumber() + "," + miTemp.getName() + "," + miTemp.getCategory() + "," +
+                        "," + miTemp.getPrice() + "," + miTemp.getQuantity() + newline);
+            }
+
+            writer.flush();
+            writer.close();
+        }catch (IOException e){
+            copyFiles(backUpFile, this.menuFile);
+            new File(backUpFile).delete();
+            e.printStackTrace();
+            return false;
+        }
+
+        //delete backup if successful
+        new File(backUpFile).delete();
+        return true;
+    }
+
+    /**
+     * Simple copy function used in the updateFile method to create a backup file
+     * @param source
+     * @param dest
+     */
+    private void copyFiles(String source, String dest){
+        FileChannel sourceChannel = null;
+        FileChannel destChannel = null;
+        try {
+            sourceChannel = new FileInputStream(source).getChannel();
+            destChannel = new FileOutputStream(dest).getChannel();
+            destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+            sourceChannel.close();
+            destChannel.close();
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
-	
-	
-
-
-public void readFile(String filename) {
-	try {
-		Scanner inputFromFile = new Scanner(new File(filename));
-		
-		  //checks if there is a line in the scanner 
-		while (inputFromFile.hasNextLine()) {
-			
-			// reads the line and assign the data read from the line to a local variable
-			String datainLine = inputFromFile.nextLine();
-			
-			// Checks if the line has any data, if no data exists in the line then stop the reading and close the file
-			if (datainLine.length() != 0) {
-				
-				//Send the read data from a line for further processing to a function called processDataInLine
-				processLine(datainLine);
-			}
-		}
-		inputFromFile.close(); //close the file
-		
-	} 
-	catch (FileNotFoundException fnf) {
-		System.out.println("File not found ");
-	}
-}
-/* find menu item by description
-public MenuItem getMenuItem(String name) {
-	return menuItems.get(name.hashCode())
-	
-}*/
-
-private Category translateCategory(String test) {
-	if(test == "hot drink") {
-		return Category.HOTDRINK;
-	}
-	
-	else if (test == "cold drink") {
-		return Category.COLDDRINK;
-	}
-	
-	else if (test == "Pastries") {
-		return Category.PASTRIES;
-	}
-	
-	else { 
-		return Category.SANDWICH;
-	}
-	
-	
-	}
-
-public void processLine(String line) {
-String itemNumber = "";
-String itemPrice = "";
-try {
-	String parts [] = line.split(",");
-	String itemName = parts [1];
-	itemNumber = parts [0];
-	System.out.println(parts[0]);
-	System.out.println(parts[1]);
-	System.out.println(parts[2]);
-	System.out.println(parts[3]);
-
-
-
-	//Integer itemNo = Integer.parseInt(itemNumber);
-	
-	Category category = translateCategory(parts[2]);
-	
-	
-	itemPrice = parts [3];
-	double Price = Double.parseDouble(parts[3]);
-	
-	
-	//BigDecimal Price = BigDecimal.parse (itemPrice);
-	int No = Integer.parseInt(itemNumber);
-	//	public MenuItem(int itemNumber, String itemName, Category itemCategory, double price ) {
-
-	MenuItem item = new MenuItem(No,itemName,category,Price);
-	menuItems.put(itemName, item);
-}
-	finally{}
-	
-}
-
-
-
-// write text to a file 
-	public void writeToFile(String filename, String report) {
-		FileWriter fw;
-		try {
-			fw = new FileWriter(filename);
-			fw.write("The report\n");
-			fw.write(report);
-			fw.close();
-		}
-		catch(FileNotFoundException fnf) {
-			System.out.println(filename + "Not found");
-			System.exit(0);
-		}
-		catch (IOException ioe) {
-			ioe.printStackTrace();
-			System.exit(1);
-		}
-	}	
-		//print to a file 
-		public void printToFile(String filename) throws IOException {
-			PrintWriter pw;
-			try { 
-				pw = new PrintWriter(filename);
-				pw.print("The report\n" );
-				pw.close();
-			}
-			catch(FileNotFoundException pfn){
-				System.out.print(filename + "Not found");
-				System.exit(2);
-				
-			}
-		}
-		
-		// read stock csv 
-		public void readStock() {
-			Menu stock = new Menu();
-			// declares file path if file is located in diff location not in the same folder
-			String filename = "StockItems.csv";
-			// reads items from file 
-			stock.readFile(filename);
-			
-		
-		 
-					
-		
-		}
-		
-/*			
-		// get in a table 
-//		public String getTableOfItemsInMenu() {
-//			String report = "Item Number    Item Name   Category   Price \n";
-//			for (MenuItem i: ) {
-//				report += String.format("%-8s", i.getNumber());
-//				report += String.format("%-17s", i.getName());
-//				report += String.format("%-10", i.getCategory());
-				report += String.format("%-10s", i.getPrice());
-				report += "\n";
-			
-				}
-			return report;
-
-
-}*/
-		}
 		
