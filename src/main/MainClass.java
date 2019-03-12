84e970293b0661b1f9111b96711446d70cc73a9b
aat4 <44607697+aat4@users.noreply.github.com>
2019-03-12 11:57:08 +0000

    moved staff thread to staff

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -37,10 +37,10 @@ public class MainClass {
 
 		}
 
-		StaffThread john = new StaffThread("John");
+	/*	StaffThread john = new StaffThread("John");
 		StaffThread lila = new StaffThread("Lila", (long) 6000.0);
 		john.start();
-		lila.start();
+		lila.start();*/
 
 
 		Menu.updateFile();
